69c998c50ccff1200dc385e1a3c828e59930b32e
Airbagjoe <kasperlaurberg@gmail.com>
2015-02-05 14:30:41 +0100

    Upload of multiverse test

---
+++ code/Tests/MultiverseTest/src/main/java/Main.java
@@ -0,0 +1,30 @@
+/**
+ * Created by Kasper on 03-02-2015.
+ */
+
+import org.multiverse.api.references.*;
+
+import java.util.Date;
+
+import static org.multiverse.api.StmUtils.*;
+
+public class Main {
+
+    public static void main(String [] args){
+        System.out.print("Hej");
+        Account a1 = new Account(1000);
+        Account a2 = new Account(500);
+        transfer(a1,a2,250);
+    }
+
+    public static void transfer(final Account from, final Account to, final int amount){
+        atomic(new Runnable(){
+            public void run(){
+                Date date = new Date();
+
+                from.incBalance(-amount, date);
+                to.incBalance(amount, date);
+            }
+        });
+    }
+}
