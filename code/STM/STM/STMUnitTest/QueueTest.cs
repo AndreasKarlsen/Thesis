f959fad1ea1e8193c1fb4669298770747e58339a
Andreas Karlsen <andreaspk.karlsen@gmail.com>
2015-02-24 10:30:15 +0100

    Added a threaded test for queue

--- code/STM/STM/STMUnitTest/QueueTest.cs
+++ code/STM/STM/STMUnitTest/QueueTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using STM.Collections;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
 
 namespace STMUnitTest
 {
@@ -18,13 +19,18 @@ namespace STMUnitTest
         public void TestCount()
         {
             Assert.AreEqual(0, _queue.Count);
-            for(var i = 0; i < 10; i++)
-            {
-                _queue.Enqueue(i);
-            }
+            this.Enqueue(10);
             Assert.AreEqual(10, _queue.Count);
             _queue.Dequeue();
             Assert.AreEqual(9, _queue.Count);
+        }
+
+        private void Enqueue(int amount)
+        {
+            for (var i = 0; i < amount; i++)
+            {
+                _queue.Enqueue(i);
+            }
         }
 
         [TestMethod]
@@ -33,6 +39,20 @@ namespace STMUnitTest
             _queue.Enqueue(1);
             
             Assert.AreEqual(1, _queue.Dequeue());
+        }
+
+        [TestMethod]
+        public void TestThreadedEnqueuing()
+        {
+            var thread1 = new Thread(() => Enqueue(100000));
+            var thread2 = new Thread(() => Enqueue(100000));
+
+            thread1.Start();
+            thread2.Start();
+            thread1.Join();
+            thread2.Join();
+
+            Assert.AreEqual(200000, _queue.Count);
         }
     }
 }
