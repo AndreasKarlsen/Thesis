5d2220d0c86694642eb6ecb07a5c360d1a73c99a
Andreas Karlsen <andreaspk.karlsen@gmail.com>
2015-03-10 13:20:04 +0100

    Moved Retry test from Program to own unittest

--- code/STM/STM/STMTester/Program.cs
+++ code/STM/STM/STMTester/Program.cs
@@ -344,37 +344,7 @@ namespace STMTester
             t1.Join();
         }
 
-        private static void TestRetry()
-        {
-            Console.WriteLine("Retry block:");
-            var result = new TMVar<int>(10);
-            var t1 = new Thread(() =>
-            {
-                var r1 = STMSystem.Atomic(() =>
-                {
-                    var tmp = result.Value;
-                    if (tmp != 12)
-                    {
-                        STMSystem.Retry();
-                    }
-                    result.Value = tmp*10;
-                    return result.Value;
-                });
-            });
 
-            var t2 = new Thread(() => STMSystem.Atomic(() =>
-            {
-                Thread.Sleep(100);
-                result.Value = 12;
-            }));
-
-            t1.Start();
-            t2.Start();
-
-            t1.Join();
-            t2.Join();
-
-        }
 
         private static void Test4()
         {
---
+++ code/STM/STM/STMUnitTest/RetryTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using STM.Implementation.Lockbased;
+
+namespace STMUnitTest
+{
+    [TestClass]
+    public class RetryTest
+    {
+        [TestMethod]
+        public void TestRetry()
+        {
+            var result = new TMVar<int>(10);
+            var t1 = new Thread(() =>
+            {
+                var r1 = STMSystem.Atomic(() =>
+                {
+                    var tmp = result.Value;
+                    if (tmp != 12)
+                    {
+                        STMSystem.Retry();
+                    }
+                    result.Value = tmp * 10;
+                    return result.Value;
+                });
+            });
+
+            var t2 = new Thread(() => STMSystem.Atomic(() =>
+            {
+                Thread.Sleep(100);
+                result.Value = 12;
+            }));
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join(); 
+        }
+    }
+}
