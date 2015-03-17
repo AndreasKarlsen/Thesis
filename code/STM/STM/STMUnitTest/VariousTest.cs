3d9b1507ca63a219335c58d1f23ada52f4f5eb76
Kasper Laurberg <kasperlaurberg@gmail.com>
2015-03-17 16:47:24 +0100

    Added a few test case for nesting and racecondtions

---
+++ code/STM/STM/STMUnitTest/VariousTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using STM.Implementation.Lockbased;
+
+namespace STMUnitTest
+{
+    [TestClass]
+    public class VariousTest
+    {
+
+    }
+}
