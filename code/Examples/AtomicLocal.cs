cefe70cab79591b7f44b4a059f85304026c149e2
Kasper Laurberg <kasperlaurberg@gmail.com>
2015-03-19 14:23:44 +0100

    Review of stm_design

---
+++ code/Examples/AtomicLocal.cs
@@ -0,0 +1,20 @@
+using System.Threading.Tasks;
+
+public class Main
+{
+    public static int x, y, z = 1;
+    public static void Main(string[] args)
+    {
+        int x = 10;
+        var y = 0;
+
+        Task.Run(() =>
+        {
+            if (x)
+            {
+                
+            }
+        });
+
+    }
+}
