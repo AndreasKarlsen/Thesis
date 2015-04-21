49f2b586d4ac6a2006ec2527dcf3569b578df1a9
Kasper Laurberg <kasperlaurberg@gmail.com>
2015-04-21 13:06:43 +0200

    Start on section on atomic field transformation

---
+++ code/Examples/Roslyn Extension/FieldTransformation.cs
@@ -0,0 +1,13 @@
+
+namespace AtomicExamples
+{
+    //Before
+    public class AtomicFieldExample
+    {
+        private atomic int field1 = 1;
+        public atomic string field2 = "Hello world!", field3;
+
+
+    }
+
+}
